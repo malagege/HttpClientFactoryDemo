8732dc9c6c68ebba0ead4a320953fc9d5b42fa73
malagege <s9714020@gmail.com>
2023-07-22 01:37:05 +0800

    NamedClient  DI 註冊

--- Program.cs
+++ Program.cs
@@ -7,6 +7,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// 可固定寫死名稱，或者從設定檔設定(我通常是寫死啦)
+string? httpClientName = builder.Configuration["TodoHttpClientName"];
+ArgumentException.ThrowIfNullOrEmpty(httpClientName);
+// 注入 httpClientName, 第二個參數 Action<HttpClient> 可做設定
+builder.Services.AddHttpClient(
+    httpClientName,
+    client =>
+    {
+        // Set the base address of the named client.
+        client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+
+        // Add a user-agent default request header.
+        client.DefaultRequestHeaders.UserAgent.ParseAdd("dotnet-docs");
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
