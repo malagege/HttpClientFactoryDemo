7c9d691e61447022c9e1d294b682db5d1a994f21
malagege <s9714020@gmail.com>
2023-07-22 01:37:38 +0800

    NamedClient 使用

--- Controllers/HttpClientDemo.cs
+++ Controllers/HttpClientDemo.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
@@ -12,15 +13,48 @@ namespace HttpClientFactoryDemo.Controllers
     public class HttpClientDemo : ControllerBase
     {
         private readonly IHttpClientFactory _httpClientFactory = null!;
+        private readonly IConfiguration _configuration = null!;
         private readonly ILogger<HttpClientDemo> _logger = null!;
 
 
 
         public HttpClientDemo(
             IHttpClientFactory httpClientFactory,
+            IConfiguration configuration,
             ILogger<HttpClientDemo> logger) =>
-            (_httpClientFactory, _logger) = (httpClientFactory, logger);
+            (_httpClientFactory, _configuration, _logger) =
+                (httpClientFactory, configuration, logger);
 
+
+        /// <summary>
+        /// 使用 Named Client
+        /// 一樣需要注入 factory 使用 CreateClient 放置你設定名稱就可以使用
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<string> GetUserTodosAsync(int userId)
+        {
+            // Create the client
+            string? httpClientName = _configuration["TodoHttpClientName"];
+            using HttpClient client = _httpClientFactory.CreateClient(httpClientName ?? "");
+
+            try
+            {
+                // Make HTTP GET request
+                // Parse JSON response deserialize into Todo type
+                string todos = await client.GetStringAsync(
+                    $"todos?userId={userId}");
+
+                return todos;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error getting something fun to say: {Error}", ex);
+            }
+
+            return "";
+        }
 
     }
 }
