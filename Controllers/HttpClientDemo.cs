72302e062ce475e66da43fa3ae17160482c731cf
malagege <s9714020@gmail.com>
2023-07-23 01:39:54 +0800

    Controller 呼叫 ITodoService 查詢

--- Controllers/HttpClientDemo.cs
+++ Controllers/HttpClientDemo.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using HttpClientFactoryDemo.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
 
@@ -11,16 +15,22 @@ namespace HttpClientFactoryDemo.Controllers
     [Route("api/[controller]")]
     public class HttpClientDemo : ControllerBase
     {
-        private readonly IHttpClientFactory _httpClientFactory = null!;
+        private readonly ITodoService _todoService = null!;
         private readonly ILogger<HttpClientDemo> _logger = null!;
 
 
 
         public HttpClientDemo(
-            IHttpClientFactory httpClientFactory,
+            ITodoService todoService,
             ILogger<HttpClientDemo> logger) =>
-            (_httpClientFactory, _logger) = (httpClientFactory, logger);
+            (_todoService, _logger) = (todoService, logger);
 
+
+        [HttpGet]
+        public async Task<Shared.Todo[]> GetUserTodosAsync(int userId)
+        {
+            return await _todoService.GetUserTodosAsync(userId);
+        }
 
     }
 }
